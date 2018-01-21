57701f47dc6f296e85b03378a42310a46dc7f06e
StarBfly <margot.blackwood@gmail.com>
2018-01-21 19:40:32 +0300

    WIP.

--- pycalc/calculation.py
+++ pycalc/calculation.py
@@ -3,13 +3,18 @@ from postfix_notation import generate_postfix_notation
 from inspect import getargspec
 
 
-def execute_operation(numberStack, Operator):
-    argspec = getargspec(Operator)
+
+
+
+def execute_operation(operator, numberStack):
+    argspec = getargspec(operator)
     operands = []
     for item in range(len(argspec.args)):
         operands.insert(0, numberStack.pop())
-    numberberStack.append(Operator(*operands))
+        print(numberStack)
+    numberStack.append(operator(*operands))
     return numberStack[-1]
+    print(numberStack[-1])
 
 
 def calculate(expression):
@@ -17,8 +22,10 @@ def calculate(expression):
     for item in generate_postfix_notation(expression):
         if type(item) is Number:
             numberStack.append(item.value)
+            print(numberStack)
         elif type(item) is Operator:
             execute_operation(item.func, numberStack)
+            print(numberStack)
         else:
             raise ValueError("Unknown type is required.")
     return numberStack[0]
--- pycalc/launcher.py
+++ pycalc/launcher.py
@@ -2,11 +2,13 @@
 import sys
 from pyparser import parse_expression
 from postfix_notation import generate_postfix_notation
-
+from calculation import calculate
 
 def main():
     if len(sys.argv) > 1:
-        print(generate_postfix_notation(parse_expression(sys.argv[1])))
+        postfix_not = (generate_postfix_notation(parse_expression(sys.argv[1])))
+        print(postfix_not)
+        print(calculate(postfix_not))
     else:
         print("ERROR: no expression given.")
 
